3d5e17a5cf261d8dac646f754985676c93f3379e
Vpetros <rawkudodes@hotmail.com>
2025-10-22 20:02:05 +0300

    feat: add ClassComponentWithState and FunctionalComponentWithState components

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 // import Card from "./components/Card.tsx";
 import Layout from "./components/Layout.tsx";
+// import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 
 function App() {
 
@@ -27,10 +29,12 @@ function App() {
             {/*<Card title="Card">*/}
             {/*</Card>*/}
             <Layout>
-                <ArrowFunctionalComponentWithPropsType
-                    title="Is an Arrow Functional Component With 2 Props"
-                    description="lorem ipsum dolor sit amet"
-                />
+                {/*<ArrowFunctionalComponentWithPropsType*/}
+                {/*    title="Is an Arrow Functional Component With 2 Props"*/}
+                {/*    description="lorem ipsum dolor sit amet"*/}
+                {/*/>*/}
+                {/*<FunctionalComponentWithState/>*/}
+                <ClassComponentWithState/>
 
             </Layout>
         </>
