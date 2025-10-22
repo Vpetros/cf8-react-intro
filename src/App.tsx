95f90b853ac8342a0173f16b66476df978aefaa3
Vpetros <rawkudodes@hotmail.com>
2025-10-22 17:44:21 +0300

    feat: add arrow functional component with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 // import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
 
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 
 function App() {
 
@@ -10,9 +11,11 @@ function App() {
     return (
         <>
             {/*<ViteIntro />*/}
-            <ClassComponent/>
-            <FunctionalComponent/>
-            <ArrowFunctionalComponent/>
+            {/*<ClassComponent/>*/}
+            {/*<FunctionalComponent/>*/}
+            {/*<ArrowFunctionalComponent/>*/}
+            <ArrowFunctionalComponentWithProps title="Is an Arrow Functional Component With Props"/>
+            <ArrowFunctionalComponentWithProps title="2nd title"/>
         </>
     )
 }
