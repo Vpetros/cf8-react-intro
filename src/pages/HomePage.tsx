717473d2d0f729e2c008bdbc42409264211abe0c
Vpetros <rawkudodes@hotmail.com>
2025-10-29 20:45:58 +0200

    feat: home with page links using map.()

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,30 @@
-import {Link, NavLink} from "react-router";
+import {Link} from "react-router";
+import {useEffect} from "react";
 
 const HomePage = () => {
+    const links = [
+        {path: "/", label: "Home Page"},
+        {path: "/name-changer", label: "Name Changer Example"},
+        {path: "/examples/timer", label: "Timer Example"}
+        ];
+
+    useEffect(()=> {
+        document.title = "CF8 Home Page";
+    }, []);
+
     return (
         <>
             <h1 className="text-2xl text-center my-12">Home Page</h1>
-            <div className="flex flex-col items-start max-w-sm max-auto gap-4">
-            <Link
-                to="/name-changer"
-                className="bg-gray-200 w-full px-4 py-2 rounded"
-            >
-                Name Changer Example
-            </Link>
-            <NavLink
-                to="/timer"
-                className="bg-gray-200 w-full px-4 py-2 rounded"
-                >
-                Timer Example
-            </NavLink>
+            <div className="flex flex-col items-start max-w-sm mx-auto gap-4">
+                {links.map((link) => (
+                    <Link
+                        key={link.path}
+                        to={link.path}
+                        className="bg-gray-200 w-full px-4 py-2 rounded"
+                    >
+                        {link.label}
+                    </Link>
+                ))}
             </div>
         </>
     )
